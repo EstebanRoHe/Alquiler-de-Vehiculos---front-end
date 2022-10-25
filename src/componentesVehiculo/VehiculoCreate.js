91149b4ce86d64b52cadebc109387b19de0ec15e
EstebanRoHe <101614805+EstebanRoHe@users.noreply.github.com>
2022-10-24 21:55:37 -0600

    Update VehiculoCreate.js

--- src/componentesVehiculo/VehiculoCreate.js
+++ src/componentesVehiculo/VehiculoCreate.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import vehiculoService from '../services/vehiculoService';
-import Tipo_VehiculoDataService from '../services/tipo_VehiculoService'
+import Tipo_VehiculoDataService from '../services/tipo_VehiculoService';
 import { Link } from 'react-router-dom';
 
 const VehiculoCreate = () => {
